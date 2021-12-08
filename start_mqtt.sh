83022884c43757bd95b05f4d14bed92b32dbbe27
Reggie <cushingreggie@gmail.com>
2021-12-08 09:11:18 +0000

    fixing control network scripts

--- start_mqtt.sh
+++ start_mqtt.sh
@@ -1,1 +1,1 @@
-docker run  -d --network="mqtt" --ip="172.20.0.2" -v $PWD/mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
+docker run  -d --network="ctrl1" --ip="172.20.0.2" -v $PWD/mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
