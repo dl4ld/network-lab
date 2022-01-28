4fc276835bf7d805fae7af6804f329dcc30bd94b
Reggie <cushingreggie@gmail.com>
2022-01-28 11:25:33 +0000

    signed commands

--- connect_nodes_to_control_network.sh
+++ connect_nodes_to_control_network.sh
@@ -2,6 +2,7 @@
 CNTS=`docker ps | grep quagga | awk '{print $1}' | tr '\n' ' '`
 IFS=' ' read -r -a array <<< "$CNTS"
 C=10
+`cat ~/PETRINET/application-javascript/wallet_org1/org1/appUser1.id | jq .credentials.certificate | sed 's/\\\n/\\n/g'  | tr -d '"' > cert.pem`
 for element in "${array[@]}"
 do
 
@@ -11,6 +12,7 @@ do
 	if [[ $H == "as"* ]]; then
 		echo "docker exec $element sh -c 'ip route del 0/0'"
 		`docker exec $element sh -c 'ip route del 0/0'`
+		`docker cp cert.pem $element:/root/cert.pem`
 		#`docker exec -d $element sh -c '/mqtt_sub.sh > /tmp/mqtt_out'`
 	fi
 	C=$((C+1))
